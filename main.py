da47fe709a544fb445acfec10ae5b3decf870961
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-03 15:28:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+w = 6
+h = 5
+spisok = [-15, -4, -2, -7, 0, 3, 5, 12, 9]
+
+matrix = []
+index = 0
+for i in range(w):
+    list = []
+    for j in range(h):
+        list.append(spisok[index % len(spisok)])
+        index += 1
+    matrix.append(list)
+
+for list in matrix:
+    for num in list:
+        print(f"|{num:{3}}", end="|")
+    print()
+
+sum = 0
+for list in matrix:
+    for num in list:
+        if num % 3 != 0:
+            sum += num
+
+print(f"суммка не кратных 3: {sum}")
